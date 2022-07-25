12f9be319f0f68a12a8c3b90e290e51b27b8b2c6
朱淑献 <zhushuxian@digitalgd.com.cn>
2022-07-25 11:34:44 +0800

    [+]传输对象模式

---
+++ src/main/java/com/justimagine/others/transferobject/StudentVO.java
@@ -0,0 +1,32 @@
+package com.justimagine.others.transferobject;
+
+/**
+ * @Description TODO
+ * @Date 2022/7/25 11:31
+ * @Created by zhushuxian
+ */
+public class StudentVO {
+    private String name;
+    private int rollNo;
+
+    StudentVO(String name, int rollNo){
+        this.name = name;
+        this.rollNo = rollNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+}
